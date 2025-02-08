20b49bb554a68bbc8650a0c3bba27724477a4897
Prashant Pandey <prashantpandey76867@gmail.com>
2025-02-08 12:29:49 +0530

    fghj

--- reactSecond/src/App.jsx
+++ reactSecond/src/App.jsx
@@ -1,23 +1,53 @@
 import Navbar from "./Navbar"
+import Card from "./Card"
+import myimg from "./t.jpg"
 
 function App() {
 
   let para = {
     color:"green",
-    fontSize:20,
+    fontSize:40,
   }
 
   return (
     <>
     <Navbar />
+
+    <hr />
+
+    <section style={{display:"flex", flexDirection:"row", justifyContent:"space-evenly", alignItems:"center"}}>
+      <h1 style={para}>Welcome to my website</h1>
+      <h2>app to aaye par aap na aaye</h2>
+      <div>
+        <img style={{width:"500px",height:"500px",border:"2px solid black",borderRadius:"50%"}} src={myimg} alt="my image" />
+      </div>
+    </section>
+
+    <hr />
       <div style={{backgroundColor:"black"}}>
       <h1 style={{color:"yellow" }}>radhe radhe</h1>
       </div>
 
       <hr />
 
+      <section style={{display:"grid",gridTemplateColumns:"auto auto auto auto"}}>
+    <Card/>
+      </section>
+      <hr />
       <p style={{color:"blue", fontSize:"22px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />A provident facere recusandae? Neque sunt adipisci iusto fuga ipsam. Iste illo velit eius tempore natus aut repellendus,<br /> odit corporis excepturi consequatur.</p>
       <p style={para}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus debitis molestiae ullam? Sed deserunt magnam omnis nulla, praesentium saepe eaque repellat in possimus dolorem ipsum, quisquam accusamus magni consequuntur libero?</p>
+
+      <section>
+        <div>
+          <img style={{width:"200px"}} src="/th.jpg" alt="" />
+          <hr />
+          <img style={{width:"200px"}} src="/src/t.jpg" alt="" />
+          <hr />
+          <img style={{width:"200px"}} src={myimg} alt="" />
+        </div>
+      </section>
+
+      
     </>
   )
 }
---
+++ reactSecond/src/Card.jsx
@@ -0,0 +1,38 @@
+const Card = ()=>{
+    return(
+
+        <>
+        <span style={{border:"2px solid black", width:"150px"}}>
+            <img src="https://tse4.mm.bing.net/th?id=OIP.HW9o-gKESImEapaUq7WbIgHaJQ&pid=Api&P=0&h=180" alt="" />
+            <h2>Card Title</h2>
+            <p>Card Description</p>
+            <button>Buy Now</button>
+        </span>
+
+        <span style={{border:"2px solid black",width:"150px"}}>
+            <img src="https://tse4.mm.bing.net/th?id=OIP.HW9o-gKESImEapaUq7WbIgHaJQ&pid=Api&P=0&h=180" alt="" />
+            <h2>Card Title</h2>
+            <p>Card Description</p>
+            <button>Buy Now</button>
+        </span>
+
+        <span style={{border:"2px solid black",width:"150px"}}>
+            <img src="https://tse4.mm.bing.net/th?id=OIP.HW9o-gKESImEapaUq7WbIgHaJQ&pid=Api&P=0&h=180" alt="" />
+            <h2>Card Title</h2>
+            <p>Card Description</p>
+            <button>Buy Now</button>
+        </span>
+
+        <span style={{border:"2px solid black",width:"150px"}}>
+            <img src="https://tse4.mm.bing.net/th?id=OIP.HW9o-gKESImEapaUq7WbIgHaJQ&pid=Api&P=0&h=180" alt="" />
+            <h2>Card Title</h2>
+            <p>Card Description</p>
+            <button>Buy Now</button>
+        </span>
+        
+        
+        </>
+    )
+}
+
+export default Card
